f26c77457f19489faeb6ba5c8d929c7195c956a3
smanaspratap <manaspratap2003@gmail.com>
2026-02-24 16:43:28 +0530

    [Manas]: Completed UC6 Password Rule2 Uppercase Check

--- LamdaExpression/LambdaExpression.java
+++ LamdaExpression/LambdaExpression.java
@@ -10,11 +10,13 @@ public class LambdaExpression {
         Validate v1 = str ->
                 str != null && str.length() >= 8;
 
+        Validate v2 = str ->str.matches(".*[A-Z].*");
+
         try(Scanner sc= new Scanner(System.in)){
             System.out.println("enter your email");
             String str= sc.nextLine();
 
-            if(v1.validate(str)){
+            if(v1.validate(str) && v2.validate(str)){
                 System.out.println("correct");
             }else{
                 System.out.println("incorrect");
