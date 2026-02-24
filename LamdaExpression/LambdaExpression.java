56722b9c61e122f29b9e94c32241861f4f601e14
smanaspratap <manaspratap2003@gmail.com>
2026-02-24 16:40:53 +0530

    [Manas]: Completed UC4 Mobile Format Validation

--- LamdaExpression/LambdaExpression.java
+++ LamdaExpression/LambdaExpression.java
@@ -7,11 +7,9 @@ interface Validate{
 
 public class LambdaExpression {
     public static void main(String[] args){
-        Validate v1 = str ->str.matches("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})+$");
-        System.out.println(v1.validate("abc@bl.in"));          // true
-        System.out.println(v1.validate("abc.xyz@bl.co.in"));   // true
-        System.out.println(v1.validate("abc..xyz@bl.co.in"));  // false
-        System.out.println(v1.validate("abc@.in"));
+        Validate v1 = mobile ->
+                mobile != null && mobile.matches("^[0-9]{2}\\s[6-9][0-9]{9}$");
+
         try(Scanner sc= new Scanner(System.in)){
             System.out.println("enter your email");
             String str= sc.nextLine();
